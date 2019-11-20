cd84f95f6c0d75f2a4c728e37cd8e31e7f5635c0
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-11-20 16:45:27 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 require 'csv'
 
 puts 'Cleaning database...'
-Bundle.delete_all
-Order.delete_all
-Customer.delete_all
-Product.delete_all
+# Bundle.delete_all
+# Order.delete_all
+# Customer.delete_all
+# Product.delete_all
 
 puts 'Creating Instances'
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'memory-tech-challenge-data.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do |row|
+csv[20201..-1].each do |row|
   customer = Customer.where(customer_id: row['customer_id']).first_or_initialize
   customer.country = row['country']
   customer.save
