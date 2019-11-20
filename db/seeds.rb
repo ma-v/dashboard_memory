29687d0ecd01ed3f15d133f1135664fb44bc0da6
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-11-20 11:52:45 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,13 @@ Customer.delete_all
 Product.delete_all
 
 puts 'Creating Instances'
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'memory-tech-challenge-data.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'memory-tech-challenge-data2.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
   customer = Customer.where(customer_id: row['customer_id']).first_or_initialize
   customer.country = row['country']
   customer.save
   product = Product.where(product_code: row['product_code']).first_or_initialize
-  product.product_description = row['product_description']
   product.product_description = row['product_description']
   product.unit_price = row['unit_price']
   product.save
